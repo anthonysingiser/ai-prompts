d155803b5e44a7d29f2a2ebd53697e0ae478d2bb
chrysanthemum <asingiser@gmail.com>
2023-08-29 09:34:36 -0400

    add signIn and signOut buttons and logic

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,6 +8,19 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
     const isUserLogged = true;
+    const [providers, setProviders] = useState(null);
+    const [toggleDropdown, setToggleDropdown] = useState(false);
+
+    useEffect(() => {
+        const setPoviders = async () => {
+            const response = await getProviders()
+            
+            setProviders(response)
+        }
+        
+        setProviders()
+    }, [])
+
     return (
         <nav className="flex-between w-full mb-16 pt-3">
             <Link href="/" className="flex gap-2 flex-center">
@@ -30,12 +43,79 @@ const Nav = () => {
                         >
                             Create Post
                         </Link>
+                        <button 
+                            type="button" 
+                            onClick={signOut}
+                            className="outline_btn"
+                        >
+                            Sign Out
+                        </button>
+                        <Link href="/profile">
+                            <Image 
+                                src="/assets/images/profile.svg"
+                                width={40}
+                                height={40}
+                                className="rounded-full object-contain"
+                                alt="profile"
+                            />
+                        </Link>
                     </div>
                  ) : (
                     <>
-                    
+                        {providers && 
+                            Object.values(providers).map((provider) => (
+                                <button
+                                    type="button"
+                                    key={provider.name}
+                                    onClick={() => signIn(provider.id)}
+                                    className="black_btn"
+                                >
+                                    Sign In
+                                </button>
+                            ))
+                        }
                     </>
                  )}
+                 <div className="sm:hidden flax relative">
+                        {isUserLogged ? (
+                            <div className="flex">
+                                <Image
+                                    src="/assets/images/logo.svg"
+                                    width={40}
+                                    height={40}
+                                    className="rounded-full"
+                                    alt="profile" 
+                                    onClick={ () => setToggleDropdown((prev) => !prev)}
+                                />
+                                {toggleDropdown && ( 
+                                    <div className="dropdown">
+                                        <Link
+                                            href="/profile"
+                                            className="dropdown_link"
+                                            onClick={() => setToggleDropdown(false)}
+                                        >
+                                            My Profile
+                                        </Link>
+                                    </div>
+                                )}
+                            </div>
+                        ):(
+                        <>
+                            {providers &&
+                                Object.values(providers).map((provider) => (
+                                    <button
+                                        type="button"
+                                        key={provider.name}
+                                        onClick={() => signIn(provider.id)}
+                                        className="black_btn"
+                                    >
+                                        Sign In
+                                    </button>
+                                ))
+                            }
+                        </>
+                    )}
+                 </div>
             </div>
         </nav>        
     )
