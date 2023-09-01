5875092f07cb92ba3f2cf9d8267fd782cbb971c7
chrysanthemum <asingiser@gmail.com>
2023-09-01 12:56:59 -0400

    create user authentication route

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { connectToDB } from "../../../utils/database";
+import  User  from "../../../models/User";
 
 const handler = NextAuth ({
     providers: [
@@ -10,16 +12,28 @@ const handler = NextAuth ({
     ],
 
     async session({ session }) {
-
+        const sessionUser = await User.findOne({ email: session.user.email })
+        session.user.id = sessionUser._id.toString();
     },
-
+    
     async signIn({ profile }) {
         try {
-            
-        } catch (error){
+            await connectToDB();
 
+        const UserExists = await User.findOne({ email: profile.email });
+        
+        if(!UserExists) {
+            await User.create({
+                email: profile.email,
+                username: profile.name.replace(" ", "" ).toLowerCase(),
+                image: profile.picture,
+            })
         }
-
+        //if not, create a new user
+        } catch (error){
+            console.log(error)
+            return false;
+        }   
     },
 })
 
