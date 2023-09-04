59836e8c725650b16cee1b022cf30bb14fb6e55c
chrysanthemum <asingiser@gmail.com>
2023-09-04 11:02:49 -0400

    merge branches

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -10,33 +10,34 @@ const handler = NextAuth ({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
+    callbacks: {
+        async session({ session }) {
+            const sessionUser = await User.findOne({ email: session.user.email })
+            session.user.id = sessionUser._id.toString();
 
-    async session({ session }) {
-        const sessionUser = await User.findOne({ email: session.user.email })
-        session.user.id = sessionUser._id.toString();
+            return session;
+        },
 
-        return session;
-    },
+        async signIn({ profile }) {
+            try {
+                await connectToDB();
 
-    async signIn({ profile }) {
-        try {
-            await connectToDB();
-
-        const UserExists = await User.findOne({ email: profile.email });
-        
-        if(!UserExists) {
-            await User.create({
-                email: profile.email,
-                username: profile.name.replace(" ", "" ).toLowerCase(),
-                image: profile.picture,
-            })
-        }
-        //if not, create a new user
-        } catch (error){
-            console.log(error)
-            return false;
-        }   
-    },
+            const UserExists = await User.findOne({ email: profile.email });
+            
+            if(!UserExists) {
+                await User.create({
+                    email: profile.email,
+                    username: profile.name.replace(" ", "" ).toLowerCase(),
+                    image: profile.picture,
+                })
+            }
+            //if not, create a new user
+            } catch (error){
+                console.log(error)
+                return false;
+            }   
+        },
+    }
 })
 
 export { handler as GET, handler as POST };
