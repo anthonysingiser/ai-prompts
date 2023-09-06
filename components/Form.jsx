885d9b79b871bfaac0e5f07d2dbf2fd605d81f20
chrysanthemum <asingiser@gmail.com>
2023-09-06 09:03:57 -0400

    finish form component

--- components/Form.jsx
+++ components/Form.jsx
@@ -32,9 +32,39 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
                         className="form_textarea"
                     />
                 </label>
+                <label>
+                    <span
+                        className="font-satoshi font-semibold text-base text-gray-700"
+                    >
+                        Prompt Field
+                        <span className="font-normal">
+                            (#goodideas, #inspiration, #creativity, #writing, #prompt)
+                        </span>
+                    </span>
+                    <input
+                        value={post.tag}
+                        onChange={(e) => setPost({ ...post,
+                        tag: e.target.value })}
+                        placeholder="#tag"
+                        required
+                        className="form_input"
+                    />
+                </label>
 
+                <div className="flex-end mx-3 mb-5 gap-4">
+                    <Link href="/" className="text-gray-500 text-sm">
+                        Cancel
+                    </Link>
+                    <button
+                        type="submit"
+                        disabled={submitting}
+                        className="px-5 py-1.5 text-sm bg-primary-orange text-white rounded-full"
+                    >
+                        {submitting ? `${type}...` : type}
+                    </button>
+                </div>
+                
             </form>
-
         </section>     
     )
 }
