8b0e34eeb9143133346a5c30c3b78cdf645d335f
chrysanthemum <asingiser@gmail.com>
2023-09-09 09:06:52 -0400

    create PrompCardList and search functionality for posts in Feed

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,59 @@
-const Feed = () => {
+'use client';
+
+import { useState, useEffect } from 'react';
+import PromptCard from './PromptCard';
+
+const PromptCardList = ({ data, handleTagClick }) => {
+
     return (
-        <div>
-            <h1>Feed</h1>
-        </div>        
+        <div className="mt-16 prompt_layout">
+            {data.map((post) => (
+                <PromptCard
+                    key={post.id}
+                    post={post}
+                    handleTagClick={handleTagClick()}
+                />
+            ))}
+        </div>
+    )
+}
+
+const Feed = () => {
+    const [searchText, setSearchText] = useState(' ')
+    const [posts, setPosts] = useState([])
+
+    const handleSearchChange = (e) => {
+        
+    }
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const response = await fetch('/api/prompt')
+            const data = await response.json()
+        }
+
+        fetchPosts()
+    }, [])
+
+    return (
+        <section>
+            <form className="relative w-full flex-center">
+                <input 
+                    type="text"
+                    placeholder="Search for a tag"
+                    value={searchText}
+                    onChange={handleSearchChange}
+                    required
+                    className="search_input peer"
+                />
+
+            </form>
+
+            <PromptCardList
+                data={posts}
+                handleTagClick={() => {}}                
+            />
+        </section>        
     )
 }
 
